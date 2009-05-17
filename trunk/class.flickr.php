52ce66c0470c31132d77d1a7e5ca8514eac1725d
jasonwtan <jasonwtan@76268ff2-f4d5-11dd-9d1a-b59b2e1864b6>
2009-05-17 04:49:00 +0000

    flickr

--- trunk/class.flickr.php
+++ trunk/class.flickr.php
@@ -5,7 +5,7 @@
 	{
 		protected $cache_ext = 'flickr';
 		protected $api_key;
-		protected $format = 'php_serial';
+		protected $format = 'json';
 		
 		function __construct($api_key)
 		{
@@ -16,13 +16,36 @@
 		function request($method, $extra = array())
 		{
 			$url = "http://api.flickr.com/services/rest/";
-			$get = array('method' => $method, 'api_key' => $this->api_key, 'format' => $this->format);
-			
+			$get = array(
+				'method' => $method,
+				'api_key' => $this->api_key,
+				'format' => $this->format
+			);
+			if ($this->format == 'json')
+				$get['nojsoncallback'] = 1;
+
 			if (isset($extra['get']) && is_array($extra['get']))
 				$extra['get'] = array_merge($extra['get'], $get);
 			
 			return parent::request($url, $extra);
 		}
+
+		function requestFeed($method, $extra = array())
+		{
+			$url = "http://api.flickr.com/services/feeds/$method.gne";
+			$get = array(
+				'lang' => 'en-us',
+				'format' => $this->format
+			);
+			if ($this->format == 'json')
+				$get['nojsoncallback'] = 1;
+
+			if (isset($extra['get']) && is_array($extra['get']))
+				$extra['get'] = array_merge($extra['get'], $get);
+			
+			return parent::request($url, $extra);
+		}
+			
 		
 		/*
 			$user_id must be NSID
@@ -35,9 +58,89 @@
 		function people_getPublicPhotos($user_id, $params = array())
 		{
 			$method = 'flickr.people.getPublicPhotos';
-			$get = array('user_id' => $user_id);
+			$get = array(
+				'user_id' => $user_id,
+				'extras' => 'date_upload,date_taken,owner_name'
+			);
+			$get = array_merge($get, $params);
+			return $this->request($method, array('get'=>$get));
+		}
+
+		function favorites_getPublicList($user_id, $params = array())
+		{
+			$method = 'flickr.favorites.getPublicList';
+			$get = array(
+				'user_id' => $user_id,
+				'extras' => 'date_upload,date_taken,owner_name'
+			);
 			$get = array_merge($get, $params);
 			return $this->request($method, array('get'=>$get));
 		}
 		
+		/* ids, tags, tagmode */
+		function feed_public($user_id, $params = array())
+		{
+			$method = 'photos_public';
+			$get = array('id' => $user_id);
+			$get = array_merge($get, $params);
+			return $this->requestFeed($method, array('get'=>$get));
+		}
 	}
+
+	class Photo
+	{
+		var	$id;
+		var	$secret;
+		var	$server;
+		var	$farm;
+		var	$size;
+		var	$owner;
+		var	$title;
+		var	$ownername;
+
+		function Photo($data)
+		{
+			$columns = array('id', 'secret', 'server', 'farm', 'owner',	'title', 'ownername', 'description');
+			foreach	($columns as $column)
+			{
+				if (is_object($data))
+				{
+					if (!isset($data->{$column}))
+						continue;
+					$this->{$column} = $data->{$column};
+				}
+				else
+				{
+					if (!isset($data[$column]))
+						continue;
+					if (is_array($data[$column]))
+						$this->{$column} = $data[$column]['_content'];
+					else
+						$this->{$column} = $data[$column];
+				}
+			}
+		}
+
+		function getImg($size =	'')	// mstb
+		{
+				if (in_array($size,	array('s', 't',	'm', 'b')))
+						$this->size	= "_$size";
+				else
+						$this->size	= "";
+				$url = "http://farm{$this->farm}.static.flickr.com/{$this->server}/{$this->id}_{$this->secret}{$this->size}.jpg";
+				return $url;
+		}
+
+		function getUrl()
+		{
+				$url = "http://www.flickr.com/photos/{$this->owner}/{$this->id}/";
+				return $url;
+		}
+
+		function getAlt()
+		{
+				$alt = str_replace('"',	'\"', $this->title)	. '	by ' . str_replace('"',	'\"', $this->ownername);
+				return $alt;
+		}
+
+}
