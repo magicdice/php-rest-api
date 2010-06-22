c4853d1ba2880ffeb0063662ce689b365874cb6f
jasonwtan <jasonwtan@76268ff2-f4d5-11dd-9d1a-b59b2e1864b6>
2010-06-22 16:11:50 +0000

    nike plus api changes

--- trunk/class.nikeplus.php
+++ trunk/class.nikeplus.php
@@ -9,7 +9,8 @@
 		
 		function getRunList($user_id, $bypass_cache = false)
 		{
-			$url = "http://nikeplus.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp?userID=" . $user_id;
+			//$url = "http://nikeplus.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp?userID=" . $user_id;
+			$url = "http://nikerunning.nike.com/nikeplus/v2/services/app/run_list.jsp?userID=" . $user_id;
 			if ($bypass_cache)
 				return $this->request($url, array('cache_life'=>1));
 			else
@@ -21,4 +22,9 @@
 			$url = "http://nikeplus.nike.com/nikeplus/v1/services/widget/get_public_run.jsp?id=$run_id&userID=$user_id";
 			return $this->request($url);
 		}
+		//1009751745
+		//410111172
+		//http://nikeplus.nike.com/nikeplus/v1/services/widget/get_public_run.jsp?id=410111172&userID=1009751745
+		//http://nikerunning.nike.com/nikeplus/v2/services/app/run_list.jsp?userID=userID
+		//http://nikerunning.nike.com/nikeplus/v2/services/app/get_run.jsp?_plus=true&id=140601564&userID=1009751745&include=metadata
 	}
